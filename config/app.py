e81a8a8791adb5bd20ed350706427d1353a3094d
rotat1ck <rotatick32151@gmail.com>
2025-04-23 02:49:29 +0300

    Изменил название функции подключающей Blueprint'ы

--- config/app.py
+++ config/app.py
@@ -5,5 +5,5 @@ class App():
     def __init__(self):
         pass
         
-    def init_api_v1(self):
+    def init_api_routes(self):
         app.register_blueprint(api_v1.user_bp, url_prefix='/api/v1/user')
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from config.config import Config
 from config.app import *
 
 if __name__ == '__main__':
-    App().init_api_v1()
+    App().init_api_routes()
     app.run(port=Config.PORT, host=Config.APP_URL)
