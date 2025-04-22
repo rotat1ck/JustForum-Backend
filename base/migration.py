f07a9d35c0b8a44c8c6037da4fbe6639d1d23d80
rotat1ck <rotatick32151@gmail.com>
2025-04-22 16:53:42 +0300

    Added class for migrations

--- base/migration.py
+++ base/migration.py
@@ -1,2 +1,10 @@
-class Migration():
-    pass
+from abc import ABC, abstractmethod
+
+class Migration(ABC):
+    @abstractmethod
+    def create(self):
+        pass
+
+    @abstractmethod
+    def drop(self):
+        pass
