74d08be47fb9b8ce89b8f95c4285ba96a4dc0b8e
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-04-25 14:55:15 +0300

    изменено

--- app/models/article_model.py
+++ app/models/article_model.py
@@ -4,8 +4,8 @@ from config import db
 class ArticleModel(Model):
     __tablename__ = 'articles' # название таблицы
     id = db.Column(db.Integer, primary_key=True) # id статьи
-    user_id = db.Column(db.Integer, unique=False, nullable=False) # id автора, юзера
-    category_id = db.Column(db.Integer, unique=True, nullable=False) # id категории
+    user_id = db.Column(db.Integer, nullable=False) # id автора, юзера
+    category_id = db.Column(db.Integer, nullable=False) # id категории
     title = db.Column(db.String(50), nullable=False) # название статьи
     description = db.Column(db.String(200), nullable=True) # описание статьи
     content = db.Column(db.Text, nullable= False) # контент статьи
