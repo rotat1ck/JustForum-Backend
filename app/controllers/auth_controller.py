089c5e1ea7fcc198238532ca84a1bd13d06a1e78
rotat1ck <rotatick32151@gmail.com>
2025-04-30 05:00:24 +0300

    AuthController | register() - добавил создание пользователя по переданным параметрам

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,15 +1,29 @@
 # registration
 from base.controller import Controller
 from app.requests.auth.register_request import RegisterRequest
+from app.models.user_model import UserModel
+
 from flask import jsonify, request
+from config import db
+from time import time
+from hashlib import sha256
 
 class AuthController(Controller):
     @staticmethod
     def register():
         reqvalid = RegisterRequest()
-        res = reqvalid.validate(request.get_json())
+        params = request.get_json()
+        res = reqvalid.validate(params)
         if res["is_failure"] != False:
             key, value = list(res["errors"][0].items())[0]
             return jsonify({key: value}), 400
         
-        return jsonify({"message": "молодец"}), 200
+        new_user = UserModel(
+            username = params.get("username"),
+            email = params.get("email"),
+            password = sha256(params.get("username").encode('utf-8')).hexdigest(),
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return jsonify({"message": "User successfully created"}), 200
